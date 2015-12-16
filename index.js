8ebdea2207c4a7f687495ff3bf6035bdd16ee51a
queicherius <queicherius@gmx.de>
2015-12-16 21:19:21 +0100

    Get it working & cleanup

--- index.js
+++ index.js
@@ -1,247 +1,155 @@
+/* eslint-disable spaced-comment */
 var restify = require('restify')
 var storage = require('node-persist')
-var engine = require('scrape-engine')
+var engine = require('scrape-engine')()
 var fs = require('fs')
 
-var self = this
-this.storage = storage
-this.engine = engine()
+/*******************************************************************
+ * Initialization
+ *******************************************************************/
 
+// Initialize node persist storage
 storage.init({
   logging: true,
   dir: __dirname + '/store'
 })
 
-var entryTemp = {}
-
-function getSA (req, res, next) {
-  storage.getItem(req.params.artist, function (err, value) {
-    // Run this code once the value has been
-    // loaded from the offline store.
-    console.log('getSA.value: ' + value)
-    console.log('req.params.artist: ' + req.params.artist)
-    var comparision = value !== undefined && req.params.artist === value.artist
-    console.log(comparision)
-    if (value !== undefined && req.params.artist === value.artist) {
-      res.send(200, value)
-    } else {
-      if (req.params.artist) {
-        self.engine.getSimilarArtist(req.params.artist, function (err, list) {
-          if (err) {
-            console.log('Error: ' + err)
-            res.status(500)
-            res.json({
-              type: false,
-              data: 'Error occured: 500'
-            })
-            return next()
-          }
-
-          console.log('list: ' + list[0].content)
-          var entry = {}
-          entry.artist = req.params.artist
-          entry.result = list
-          self.storage.setItem(entry.artist, entry, function (err, value) {
-            if (err) {
-              console.log('Error: ' + err)
-              res.status(500)
-              res.json({
-                type: false,
-                data: 'Error occured: 500'
-              })
-              return next()
-            }
-            res.send(200, entry)
-          })
-        })
-      } else {
-        console.log('Error: ' + err)
-        res.status(500)
-        res.json({
-          type: false,
-          data: 'Error occured: 500'
-        })
-      }
-    }
-    return next()
-  })
-}
-
-function getST (req, res, next) {
-  storage.getItem(req.params.titel, function (err, value) {
-    // Run this code once the value has been
-    // loaded from the offline store.
-    console.log('getST.value: ' + value)
-    console.log('req.params.titel: ' + req.params.titel)
-    var comparision = value !== undefined && req.params.titel === value.titel
-    console.log(comparision)
-    if (value !== undefined && req.params.titel === value.titel) {
-      res.send(200, value)
-    } else {
-      if (req.params.titel) {
-        self.engine.getSimilarTitel(req.params.titel, function (err, list) {
-          if (err) {
-            console.log('Error: ' + err)
-            res.status(500)
-            res.json({
-              type: false,
-              data: 'Error occured: 500'
-            })
-            return next()
-          }
-
-          console.log('list: ' + list[0].content)
-          var entry = {}
-          entry.titel = req.params.titel
-          entry.result = list
-          self.storage.setItem(entry.titel, entry, function (err, value) {
-            if (err) {
-              console.log('Error: ' + err)
-              res.status(500)
-              res.json({
-                type: false,
-                data: 'Error occured: 500'
-              })
-              return next()
-            }
-            res.send(200, entry)
-          })
-        })
-      } else {
-        console.log('Error: ' + err)
-        res.status(500)
-        res.json({
-          type: false,
-          data: 'Error occured: 500'
-        })
-      }
-    }
-    return next()
-  })
-}
-
-function getCover (req, res, next) {
-  storage.getItem('Cover:' + req.params.album, function (err, value) {
-    // Run this code once the value has been
-    // loaded from the offline store.
-    if (err) {
-      console.log('Error: ' + err)
-      res.status(500)
-      res.json({
-        type: false,
-        data: 'Error occured: 500'
-      })
-      return next()
-    }
-
-    var comparision = value !== undefined && req.params.album === value.album
-    console.log(comparision)
-    if (value !== undefined && (req.params.album) === value.album) {
-      res.send(200, value)
-    } else {
-      if (req.params.album) {
-        self.engine.getCoverURL(req.params.artist, req.params.album, function (err, list) {
-          if (err) {
-            console.log('Error: ' + err)
-            res.status(500)
-            res.json({
-              type: false,
-              data: 'Error occured: 500'
-            })
-            return next()
-          }
-
-          console.log('list: ' + list[0].content)
-          var entry = {}
-          entry.album = req.params.album
-          entry.result = list
-          self.storage.setItem('Cover:' + entry.album, entry, function (err, value) {
-            if (err) {
-              console.log('Error: ' + err)
-              res.status(500)
-              res.json({
-                type: false,
-                data: 'Error occured: 500'
-              })
-              return next()
-            }
-            res.send(200, entry)
-          })
-        })
-      } else {
-        console.log('Error: ' + err)
-        res.status(500)
-        res.json({
-          type: false,
-          data: 'Error occured: 500'
-        })
-      }
-    }
-    return next()
-  })
-}
-
-function getCoverFile (req, res, next) {
-  var filename = './' + req.params.artist + '_' + req.params.album
-  fs.readFile(filename, function (err, data) {
-    if (data !== undefined) {
-      res.send(200, data)
-    } else {
-      self.engine.getCover(req.params.artist, req.params.album, function (err, file) {
-        if (err) {
-          console.log('Error: ' + err)
-          res.status(500)
-          res.json({
-            type: false,
-            data: 'Error occured: 500'
-          })
-          return next()
-        }
-
-        console.log(file)
-        fs.writeFile(filename, file, function (err) {
-          if (err) {
-            console.log('Error: ' + err)
-            res.status(500)
-            res.json({
-              type: false,
-              data: 'Error occured: 500'
-            })
-            return next()
-          }
-          console.log('The file got saved!')
-          res.send(200, file)
-        })
-      })
-    }
-    return next()
-  })
-}
-
+// Initialize restify
 var server = restify.createServer()
 server.use(restify.queryParser())
 server.use(restify.bodyParser())
 
-// Artist
-// server.post('/similarArtist', setSA)
-server.get('/similarArtist/:artist', getSA)
+// Setup the routes
+server.post('/similarTrack', getSimilarTrack)
+server.post('/similarArtist', getSimilarArtist)
+server.post('/Cover', getCover)
 
-// Track
-server.post('/similarTrack', function create (req, res, next) {
-  entryTemp = req.body
-  res.send(201, entryTemp)
-  return next()
-})
-server.get('/similarTrack/:titel', getST)
-
-// Cover
-server.post('/Cover', function create (req, res, next) {
-  entryTemp = req.body
-  res.send(201, entryTemp)
-  return next()
-})
-server.get('/Cover/:artist/:album', getCoverFile)
-
+// Start the server
 server.listen(8080, '127.0.0.1', function () {
   console.log('%s listening at %s', server.name, server.url)
 })
+
+/*******************************************************************
+ * Controllers
+ *******************************************************************/
+
+// Get the cover for an album
+function getCover (request, response, next) {
+  // Get the request from the payload
+  var payload = JSON.parse(request.body).payload
+  var artist = payload.artist
+  var album = payload.album
+  var filename = './' + artist + '_' + album
+
+  // Check if we already have that in cache
+  fs.readFile(filename, function (err, data) {
+    // We already have the file in cache, send it back to the user
+    if (data !== undefined) {
+      response.send(200, data)
+      return next()
+    }
+
+    // We need to get the data from our engine
+    engine.getCover(album, artist, function (err, file) {
+      if (err) {
+        return errorLogger(err, response, next)
+      }
+
+      // We got the cover from our engine, let's save it and return
+      fs.writeFile(filename, file, 'binary', function (err) {
+        if (err) {
+          return errorLogger(err, response, next)
+        }
+        console.log('The file "' + filename + '" got saved!')
+        response.send(200, file)
+        return next()
+      })
+    })
+  })
+}
+
+// Get similar tracks
+function getSimilarTrack (request, response, next) {
+  // Get the request from the payload
+  var payload = JSON.parse(request.body).payload
+  var title = payload.title
+
+  if (!title) {
+    return errorLogger('Title is not defined', response, next)
+  }
+
+  // Check if we already have that in cache
+  storage.getItem(title, function (err, cache) {
+    // We already have the file in cache, send it back to the user
+    if (cache !== undefined && title === cache.titel) {
+      response.send(200, cache)
+      return next()
+    }
+
+    // We need to get the data from our engine
+    engine.getSimilarTitel(title, function (err, result) {
+      if (err) {
+        return errorLogger(err, response, next)
+      }
+
+      // We got the data from our engine, let's save it and return
+      var entry = {title: title, result: result}
+      storage.setItem(entry.title, entry, function (err) {
+        if (err) {
+          return errorLogger(err, response, next)
+        }
+        response.send(200, entry)
+        return next()
+      })
+    })
+  })
+}
+
+// Get similar artists
+function getSimilarArtist (request, response, next) {
+  // Get the request from the payload
+  var payload = JSON.parse(request.body).payload
+  var artist = payload.artist
+
+  if (!artist) {
+    return errorLogger('Artist is not defined', response, next)
+  }
+
+  // Check if we already have that in cache
+  storage.getItem(artist, function (err, cache) {
+    // We already have the file in cache, send it back to the user
+    if (cache !== undefined && artist === cache.artist) {
+      response.send(200, cache)
+      return next()
+    }
+
+    // We need to get the data from our engine
+    engine.getSimilarArtist(artist, function (err, result) {
+      if (err) {
+        return errorLogger(err, response, next)
+      }
+
+      // We got the data from our engine, let's save it and return
+      result = result.map(function (x) { return x.content })
+      var entry = {artist: artist, result: result}
+      storage.setItem(entry.artist, entry, function (err) {
+        if (err) {
+          return errorLogger(err, response, next)
+        }
+        response.send(200, entry)
+        return next()
+      })
+    })
+  })
+}
+
+// Log an error out to the user
+function errorLogger (err, response, next) {
+  console.error('Error: ' + err)
+  response.status(500)
+  response.json({
+    type: false,
+    data: 'Error occurred: ' + err
+  })
+  return next()
+}
