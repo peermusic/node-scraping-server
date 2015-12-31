2be584a2a49514594e27d85a8aef23e2efc8040d
queicherius <queicherius@gmail.com>
2015-12-31 13:11:50 +0100

    Make sure that our storage is always in sync and remove unused code

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ var rusha = new (require('rusha'))()
  * Initialization
  *******************************************************************/
 
-// Initialize node persist storage
-storage.init({dir: __dirname + '/store'})
-
 // Initialize restify
 var server = restify.createServer()
 server.use(restify.queryParser())
@@ -21,7 +18,6 @@ server.use(restify.fullResponse())
 
 // Setup the routes
 server.post('/similarTrack', getSimilarTrack)
-server.post('/similarArtist', getSimilarArtist)
 server.post('/Cover', getCover)
 
 // Start the server
@@ -35,6 +31,9 @@ server.listen(8080, '127.0.0.1', function () {
 
 // Get the cover for an album
 function getCover (request, response, next) {
+  // Make sure our storage is synchronized with what we have on disk
+  storage.initSync({dir: __dirname + '/store'})
+
   // Get the request from the payload
   var payload = request.body.payload
   var artist = payload.artist
@@ -79,9 +78,12 @@ function getCover (request, response, next) {
 
 // Get similar tracks
 function getSimilarTrack (request, response, next) {
+  // Make sure our storage is synchronized with what we have on disk
+  storage.initSync({dir: __dirname + '/store'})
+
   // Get the request from the payload
   var payload = request.body.payload
-  var hash = rusha.digestFromString(JSON.stringify(payload))
+  var hash = 'similar-track' + rusha.digestFromString(JSON.stringify(payload))
 
   // Check if we already have that in cache
   storage.getItem(hash, function (err, cache) {
@@ -100,44 +102,6 @@ function getSimilarTrack (request, response, next) {
       // We got the data from our engine, let's save it and return
       var entry = {payload: payload, result: result}
       storage.setItem(hash, entry, function (err) {
-        if (err) {
-          return errorLogger(err, response, next)
-        }
-        response.send(200, entry)
-        return next()
-      })
-    })
-  })
-}
-
-// Get similar artists
-function getSimilarArtist (request, response, next) {
-  // Get the request from the payload
-  var payload = request.body.payload
-  var artist = payload.artist
-
-  if (!artist) {
-    return errorLogger('Artist is not defined', response, next)
-  }
-
-  // Check if we already have that in cache
-  storage.getItem(artist, function (err, cache) {
-    // We already have the file in cache, send it back to the user
-    if (cache !== undefined && artist === cache.artist) {
-      response.send(200, cache)
-      return next()
-    }
-
-    // We need to get the data from our engine
-    engine.getSimilarArtist(artist, function (err, result) {
-      if (err) {
-        return errorLogger(err, response, next)
-      }
-
-      // We got the data from our engine, let's save it and return
-      result = result.map(function (x) { return x.content })
-      var entry = {artist: artist, result: result}
-      storage.setItem(entry.artist, entry, function (err) {
         if (err) {
           return errorLogger(err, response, next)
         }
